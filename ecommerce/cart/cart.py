209c2aebfef290bdcc4260bd868c2c4becd3b5a3
Your Name <9778060@gmail.com>
2025-01-11 20:06:53 +0200

    shopping cart optimization 2

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -10,75 +10,57 @@ class Cart:
         if "session_details" not in self.session:
             self.session["session_details"] = {}
 
-        self.session_details = self.session.get("session_details")
+        self.shopping_cart = deepcopy(self.session.get("session_details"))
         self._cart_validation()
 
 
     def _cart_validation(self):
-        all_product_keys = self.session_details.keys()
+        all_product_keys = self.shopping_cart.keys()
         products = Product.objects.filter(id__in=all_product_keys, show=True)
 
         for product in products:
-            self.session_details[str(product.id)]["product"] = product
-            self.session_details[str(product.id)]["total"] = str(Decimal(product.price) * self.session_details[str(product.id)]["qty"])
+            self.shopping_cart[str(product.id)]["product"] = product
+            self.shopping_cart[str(product.id)]["total"] = str(Decimal(product.price) * self.shopping_cart[str(product.id)]["qty"])
 
-        copy_of_cart = self.session_details.copy()
+        copy_of_cart = self.shopping_cart.copy()
 
         for key, value in copy_of_cart.items():
             try:
                 value["product"]
             except KeyError as exc:
-                del self.session_details[key]
+                del self.shopping_cart[key]
 
-        for key in self.session_details:
+        self.session["session_details"] = deepcopy(self.shopping_cart)
+
+        for key in self.session["session_details"]:
             try:
-                del self.session_details[key]["product"]
+                del self.session["session_details"][key]["product"]
             except KeyError as exc:
                 continue
-
-        self.session["session_details"] = self.session_details
-
-
-    def _cart_enhancement(self):
-        all_product_keys = self.session_details.keys()
-        products = Product.objects.filter(id__in=all_product_keys, show=True)
-        ammended_cart = deepcopy(self.session_details)
-
-        for product in products:
-            ammended_cart[str(product.id)]["product"] = product
-            ammended_cart[str(product.id)]["total"] = str(Decimal(product.price) * ammended_cart[str(product.id)]["qty"])
-
-        copy_of_cart = ammended_cart.copy()
-
-        for key, value in copy_of_cart.items():
-            try:
-                value["product"]
-            except KeyError as exc:
-                del ammended_cart[key]
-
-        return ammended_cart
 
 
     def add_update_cart(self, product_id, product_qty):
         dict_to_add = {product_id: {"qty": product_qty}}
-        self.session_details.update(dict_to_add)
+        self.session["session_details"].update(dict_to_add)
         return dict_to_add
 
 
     def delete_from_cart(self, product_id):
-        if product_id in self.session_details:
-            del self.session_details[product_id]
+        try:
+            del self.session["session_details"][product_id]
+        except KeyError as exc:
+            pass
 
 
     @property
     def cart_total_amount(self):
-        return f"{round(sum((Decimal(value["total"]) for value in self.session_details.values())), 2):.2f}"
+        return f"{round(sum((Decimal(value["total"]) for value in self.shopping_cart.values())), 2):.2f}"
 
 
     def __len__(self):
-        return sum((value["qty"] for value in self.session_details.values()))
+        return sum((value["qty"] for value in self.shopping_cart.values()))
 
 
     def __iter__(self):
-        yield from self._cart_enhancement().values()
+        yield from self.shopping_cart.values()
  
