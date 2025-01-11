6c2fe26467126553937791eeb1d071bc040ff9a4
Your Name <9778060@gmail.com>
2025-01-11 19:53:39 +0200

    shopping cart optimization 1

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -11,13 +11,63 @@ class Cart:
             self.session["session_details"] = {}
 
         self.session_details = self.session.get("session_details")
-       
+        self._cart_validation()
 
-    def add_to_cart(self, product_id, product_qty):
-        dict_to_add = {str(product_id): {"qty": product_qty}}
-        self.session_details.update(dict_to_add)
+
+    def _cart_validation(self):
+        all_product_keys = self.session_details.keys()
+        products = Product.objects.filter(id__in=all_product_keys, show=True)
+
+        for product in products:
+            self.session_details[str(product.id)]["product"] = product
+            self.session_details[str(product.id)]["total"] = str(Decimal(product.price) * self.session_details[str(product.id)]["qty"])
+
+        copy_of_cart = self.session_details.copy()
+
+        for key, value in copy_of_cart.items():
+            try:
+                value["product"]
+            except KeyError as exc:
+                del self.session_details[key]
+
+        for key in self.session_details:
+            try:
+                del self.session_details[key]["product"]
+            except KeyError as exc:
+                continue
+
         self.session["session_details"] = self.session_details
+
+
+    def _cart_enhancement(self):
+        all_product_keys = self.session_details.keys()
+        products = Product.objects.filter(id__in=all_product_keys, show=True)
+        ammended_cart = deepcopy(self.session_details)
+
+        for product in products:
+            ammended_cart[str(product.id)]["product"] = product
+            ammended_cart[str(product.id)]["total"] = str(Decimal(product.price) * ammended_cart[str(product.id)]["qty"])
+
+        copy_of_cart = ammended_cart.copy()
+
+        for key, value in copy_of_cart.items():
+            try:
+                value["product"]
+            except KeyError as exc:
+                del ammended_cart[key]
+
+        return ammended_cart
+
+
+    def add_update_cart(self, product_id, product_qty):
+        dict_to_add = {product_id: {"qty": product_qty}}
+        self.session_details.update(dict_to_add)
         return dict_to_add
+
+
+    def delete_from_cart(self, product_id):
+        if product_id in self.session_details:
+            del self.session_details[product_id]
 
 
     @property
@@ -30,34 +80,5 @@ class Cart:
 
 
     def __iter__(self):
-        all_product_keys = self.session_details.keys()
-        products = Product.objects.filter(id__in=all_product_keys, show=True)
-        ammended_cart = self.session_details.copy()
-
-        for product in products:
-            ammended_cart[str(product.id)]["product"] = product
-            ammended_cart[str(product.id)]["price"] = Decimal(product.price)
-            ammended_cart[str(product.id)]["total"] = str(ammended_cart[str(product.id)]["price"] * ammended_cart[str(product.id)]["qty"])
-
-        copy_of_cart = ammended_cart.copy()
-
-        for key, value in copy_of_cart.items():
-            try:
-                value["product"]
-            except KeyError as exc:
-                del ammended_cart[key]
-
-        self.session_details = deepcopy(ammended_cart)
-        
-        for key in self.session_details:
-            try:
-                del self.session_details[key]["product"]
-                del self.session_details[key]["price"]
-            except KeyError as exc:
-                continue
-
-        self.session["session_details"] = self.session_details
-
-
-        yield from ammended_cart.values()
+        yield from self._cart_enhancement().values()
  
--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -13,23 +13,40 @@ def cart_add(request):
 
     if request.POST.get("action") == "post":
         existing_cart = Cart(request)
-        product_id = int(request.POST.get("product_id"))
+        product_id = str(request.POST.get("product_id"))
         product_qty = int(request.POST.get("product_qty"))
 
         # if selected_product := get_object_or_404(Product, id=product_id, show=True):
         #     dict_to_add = existing_cart.add_to_cart(selected_product, product_qty)
         #     response = JsonResponse(dict_to_add)
 
-        dict_to_add = existing_cart.add_to_cart(product_id, product_qty)
+        dict_to_add = existing_cart.add_update_cart(product_id, product_qty)
         response = JsonResponse(dict_to_add)
 
     return response
 
 
 def cart_delete(request):
-    pass
+    response = JsonResponse({"empty": None})
+
+    if request.POST.get("action") == "post":
+        existing_cart = Cart(request)
+        product_id = str(request.POST.get("product_id"))
+
+        existing_cart.delete_from_cart(product_id)
+
+    return response
 
 
 def cart_update(request):
-    pass
+    response = JsonResponse({"empty": None})
 
+    if request.POST.get("action") == "post":
+        existing_cart = Cart(request)
+        product_id = str(request.POST.get("product_id"))
+        product_qty = int(request.POST.get("product_qty"))
+
+        dict_to_update = existing_cart.add_update_cart(product_id, product_qty)
+        response = JsonResponse(dict_to_update)
+
+    return response
