adbac1a0506023055a8463ca940153ce66388824
Your Name <9778060@gmail.com>
2025-03-02 19:01:09 +0200

    order start

--- ecommerce/payment/views.py
+++ ecommerce/payment/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
-from payment.models import ShippingAddress
+from payment.models import ShippingAddress, Order, OrderItem
 from payment.forms import ShippingForm
 from django.contrib import messages
+from cart.cart import Cart
 
 
 
 def checkout(request):
+
+    cart = Cart(request)
+    if not len(cart):
+        return redirect("dashboard")
 
     try:
         shipping_address = ShippingAddress.objects.filter(user=request.user.id).order_by("-date_created")[:1].get()
@@ -24,21 +29,61 @@ def checkout(request):
             if request.user and request.user.is_authenticated and shipping_address_form.changed_data:
                 shipping_address_form.instance.user = request.user
                 shipping_address_form.save()
-            
-            messages.add_message(request, messages.SUCCESS, "Checkout completed")
 
-            context = {}
-            return render(request, "payment_success.html", context=context)
+            form = shipping_address_form
+
+            if request.user and request.user.is_authenticated:
+                full_name = f"{request.user.first_name} {request.user.last_name}"
+                email = request.user.email
+            else:
+                full_name = request.POST.get("full_name")
+                email = request.POST.get("email")
+
+            context = {"form": form, "full_name": full_name, "email": email}
+            return render(request, "complete_order.html", context=context)
 
         else:
             messages.add_message(request, messages.ERROR, "Unable to proceed with checkout")
-            # context = {}
-            # return render(request, "payment_fail.html", context=context)
 
     form = shipping_address_form
     context = {"form": form}
 
     return render(request, "checkout.html", context=context)
+
+
+def complete_order(request):
+    cart = Cart(request)
+    if not len(cart):
+        return redirect("dashboard")
+    
+    if request.method == "POST":
+        full_name = request.POST.get("full_name")
+        email = request.POST.get("email")
+
+        address1 = request.POST.get("address1")
+        address2 = request.POST.get("address2")
+        city = request.POST.get("city")
+        state = request.POST.get("state")
+        zipcode = request.POST.get("zipcode")
+        country = request.POST.get("country")
+
+        shipping_address = f"{address1}\n{address2}\n{city}, {state}\n, {zipcode}, {country}"
+
+        amount_to_pay = cart.cart_total_amount
+
+        user = request.user if request.user.is_authenticated else None
+
+        order = Order.objects.create(
+            full_name=full_name,
+            email=email,
+            shipping_address=shipping_address,
+            amount_paid=amount_to_pay,
+            user=user
+        )
+
+
+    return redirect("dashboard")
+    
 
 
 def payment_success(request):
